b16ddfca52676bda4ebd8d7bf8c955e6ebc0bffd
Project Owner <jonas.rapp@innofactor.com>
2017-02-17 10:01:29 +0100

    Add project files.

---
+++ git-essentials/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_essentials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
